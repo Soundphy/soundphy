f2b32df485e7b9835d31aff7d26ef35111d1ffd9
Miguel Sánchez de León Peque <peque@neosit.es>
2016-10-24 21:35:59 +0200

    Make default logo size smaller

--- docs/source/logo/logo.py
+++ docs/source/logo/logo.py
@@ -11,11 +11,11 @@ y1 = numpy.sin(x1)
 x = x0
 y = y0 * y1
 
-fig = plt.figure(figsize=(2, 2), facecolor='purple')
-plt.plot(x, y, color='w', linewidth=4.0)
+fig = plt.figure(figsize=(1, 1), facecolor='purple')
+plt.plot(x, y, color='w', linewidth=2.0)
 plt.axis('off')
 fig.axes[0].get_xaxis().set_visible(False)
 fig.axes[0].get_yaxis().set_visible(False)
 
-plt.savefig('logo.svg', facecolor='purple', bbox_inches='tight', pad_inches=.5)
-plt.savefig('logo.png', facecolor='purple', bbox_inches='tight', pad_inches=.5)
+for name in ('logo.svg', 'logo.png'):
+    plt.savefig(name, facecolor='purple', bbox_inches='tight', pad_inches=.25)
