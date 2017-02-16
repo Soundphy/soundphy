07c60c0ca554b3d63faa8123cf44a49201fbe61d
Xurxo Fresco <xurxof@gmail.com>
2017-02-16 21:44:24 +0100

    test: get non-existent sound get

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -19,3 +19,16 @@ def test_sounds_empty_database(client):
     response = client.get('/sounds/')
     assert response.status_code == 200
     assert decode(response) == []
+
+
+
+@pytest.mark.django_db
+def test_sounds_number_empty_database(client):
+    """
+    Get a no existent sound in an empty database should result in 301 error.
+    Any client can make this request (no authentication needed).
+    """
+    response = client.get('/sounds/1')
+    assert response.status_code == 301
+    assert decode(response) == []
+
